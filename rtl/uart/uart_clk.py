1169bcef9ae777e32ce77718d224837b489d2dc9
Angel Terrones <angelterrones@gmail.com>
2017-11-03 14:13:40 -0400

    UART clk.

--- rtl/uart/uart_clk.py
+++ rtl/uart/uart_clk.py
@@ -2,15 +2,26 @@
 # Copyright (c) 2017 Angel Terrones <aterrones@usb.ve>
 
 import myhdl as hdl
-from rtl.utils import createSignal
-from rtl.utils import log2up
+from coregen.utils import createSignal
+from coregen.utils import log2up
 
 
 @hdl.block
-def clk_div(clk_i, rst_i, uart_tick, uart_tick_16x, CLK_BUS=50000000, BAUD_RATE=115200):
-    _divisor = CLK_BUS//(BAUD_RATE * 16)  # clk base for rx (x16)
+def clk_div(clk_i, rst_i, uart_tick, uart_tick_x16, CLK_BUS=50000000, BAUD_RATE=115200):
+    _divisor = CLK_BUS // BAUD_RATE
+    counter  = createSignal(0, log2up(_divisor))
 
-    counter = createSignal(0, log2up(_divisor))
+    @hdl.always_seq(clk_i.posedge, reset=rst_i)
+    def counter_proc():
+        if counter == _divisor - 1:
+            counter.next = 0
+        else:
+            counter.next = counter + 1
+
+    @hdl.always_seq(clk_i.posedge, reset=rst_i)
+    def uart_tick_proc():
+        uart_tick.next     = counter == 0
+        uart_tick_x16.next = counter[len(counter) - 4:0] == 0
 
     return hdl.instances()
 
