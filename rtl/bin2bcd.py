c63ff3687069b98adf0ea7ade9072e14018d97f1
Angel Terrones <angelterrones@gmail.com>
2017-11-07 22:14:59 -0400

    Working bin2bcd.
    Sometimes, I hate myHDL.

--- rtl/bin2bcd.py
+++ rtl/bin2bcd.py
@@ -1,54 +1,49 @@
 #!/usr/bin/env python3
-# Copyright (c) 2017 Angel Terrones <aterrones@usb.ve>
+# Copyright (ten) 2017 Angel Terrones <aterrones@usb.ve>
 
 import myhdl as hdl
+from coregen.utils import createSignal
 
 
 @hdl.block
-def bin2bcd(binary, thousands, hundreds, tens, ones):
+def bin2bcd(clk_i, rst_i, binary, thousands, hundreds, tens, ones):
     assert len(binary) <= 14, "[bin2bcd] Error: max length of 'binary' is 14. Wrong value: {1}".format(len(binary))
     assert len(thousands) == 4, "[bin2bcd] Error: len(thousands) must be 4"
     assert len(hundreds) == 4, "[bin2bcd] Error: len(hundreds) must be 4"
     assert len(tens) == 4, "[bin2bcd] Error: len(tens) must be 4"
     assert len(ones) == 4, "[bin2bcd] Error: len(ones) must be 4"
 
-    @hdl.always_comb
+    NBIT  = len(binary)
+    shift = [createSignal(0, 4*4+NBIT) for _ in range(NBIT + 1)]
+
+    @hdl.always_seq(clk_i.posedge, reset=rst_i)
     def decomp_proc():
-        thousands.next = 0
-        hundreds.next  = 0
-        tens.next      = 0
-        ones.next      = 0
-        for i in range(len(binary)):
-            if thousands >= 5:
-                thousands.next = thousands + 3
-            if hundreds >= 5:
-                hundreds.next = hundreds + 3
-            if tens >= 5:
-                tens.next = tens + 3
-            if ones >= 5:
-                ones.next = ones + 3
-            # shift left one
-            thousands.next    = thousands << 1
-            thousands[0].next = hundreds[3]
-            hundreds.next     = hundreds << 1
-            hundreds[0].next  = tens[3]
-            tens.next         = tens << 1
-            tens[0].next      = ones[3]
-            ones.next         = ones << 1
-            ones[0].next      = binary[i]
+        """ verilator lint_off WIDTH """
+        shift[0].next  = binary
+        for i in range(NBIT):
+            thousand = int(shift[i][NBIT + 16:NBIT + 12])
+            hundred  = int(shift[i][NBIT + 12:NBIT + 8])
+            ten      = int(shift[i][NBIT + 8:NBIT + 4])
+            one      = int(shift[i][NBIT + 4:NBIT])
+            if thousand >= 5:
+                thousand = thousand + 3
+            if hundred >= 5:
+                hundred = hundred + 3
+            if ten >= 5:
+                ten = ten + 3
+            if one >= 5:
+                one = one + 3
+            shift[i + 1].next = hdl.concat(hdl.modbv(thousand)[4:], hdl.modbv(hundred)[4:], hdl.modbv(ten)[4:], hdl.modbv(one)[4:], shift[i][NBIT:]) << 1
+        """ verilator lint_on WIDTH """
+
+    @hdl.always_seq(clk_i.posedge, reset=rst_i)
+    def assign_proc():
+        thousands.next = shift[NBIT][NBIT + 16:NBIT + 12]
+        hundreds.next  = shift[NBIT][NBIT + 12:NBIT + 8]
+        tens.next      = shift[NBIT][NBIT + 8:NBIT + 4]
+        ones.next      = shift[NBIT][NBIT + 4:NBIT]
 
     return hdl.instances()
-
-
-if __name__ == '__main__':
-    from coregen.utils import createSignal
-    b   = createSignal(0, 10)
-    t   = createSignal(0, 4)
-    h   = createSignal(0, 4)
-    tn  = createSignal(0, 4)
-    o   = createSignal(0, 4)
-    dut = bin2bcd(b, t, h, tn, o)
-    dut.convert(testbench=False)
 
 # Local Variables:
 # flycheck-flake8-maximum-line-length: 200
